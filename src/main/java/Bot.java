84d9b1bfd98cd6072f2ccefc9898113f0c1b4790
Alexander Slesarev <alexander.g.slesarev@gmail.com>
2022-04-05 00:11:42 +0300

    Add functionality

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,26 +4,53 @@ import org.json.simple.parser.ParseException;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Bot extends TelegramLongPollingBot {
+    TInvestApi investApi;
+
+    Bot() {
+        investApi = new TInvestApi();
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String message_text = update.getMessage().getText();
-            String chat_id = update.getMessage().getChatId().toString();
-
-            SendMessage message = new SendMessage();
-            message.setChatId(chat_id);
-            message.setText(message_text);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        SendMessage message = new SendMessage();
+        try {
+            String chatId = update.getMessage().getChatId().toString();
+            message.setChatId(chatId);
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                String[] request = update.getMessage().getText().split("\\s+");
+                String response = null;
+                response = parseMessage(request);
+                message.setText(response);
+            } else {
+                message.setText("Server error");
             }
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String parseMessage(String[] request) throws Exception {
+        String command = request[0];
+        if (command.equals("/currencies")) {
+            return investApi.totalAmountCurrencies();
+        } else if (command.equals("/shares")) {
+            return investApi.totalAmountShares();
+        } else if (command.equals("/figi")) {
+            return investApi.instrumentByFigi(request[1]);
+        } else if (command.equals("/newstop")) {
+            return investApi.newStopOrder(request[1], request[2]);
+        } else if (command.equals("/withdrawstop")) {
+            return investApi.withdrawStopOrder(request[1]);
+        }  else if (command.equals("/liststop")) {
+            return investApi.stopOrderList();
+        }else {
+            throw new Exception("Parse error");
         }
     }
 
